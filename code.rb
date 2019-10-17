1c1b474647f6d97fb94fcbda98016f513d7d1c71
Murad02 <56655044+Murad02@users.noreply.github.com>
2019-10-17 12:21:17 +0400

    Add files via upload

---
+++ code.rb
@@ -0,0 +1,10 @@
+print "Enter Your Code : "
+ x=gets.chomp.to_i
+ while 
+    x!=123
+ 	print "Your pass is not match Enter Again : " 
+ 	x=gets.chomp.to_i
+ end
+ 	if x==123
+ 		puts "You Entered"
+ 	end
